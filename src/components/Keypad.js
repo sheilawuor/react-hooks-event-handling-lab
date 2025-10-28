3010f85bc27031e6683806ee6cec09039bfb7969
sheilawuor <sheilawuor01@gmail>
2025-10-28 09:47:35 +0300

    add all

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
-// Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password" 
+                onChange={() => console.log('Entering password...')} 
+            />
+        </div>
     )
 }
 
